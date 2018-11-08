ed300ab87b5de20fb0f69584c0eddb56cd10e659
Ali <Ali@192.168.1.109>
2018-11-07 19:59:46 -0600

    Updates to Shoe project.

---
+++ Shoe/src/shoe/Card.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shoe;
+
+/**
+ *
+ * @author Ali
+ */
+public class Card {
+    
+}
