5516eb70603e9f490f1c368123599e69ac655f96
Ali <Ali@192.168.1.109>
2018-12-17 14:58:30 -0600

    Added further support for aces to be treated as one or eleven.

--- Shoe/src/shoe/Hand.java
+++ Shoe/src/shoe/Hand.java
@@ -12,16 +12,24 @@ public class Hand {
     
     private List <Card> cardValue = new ArrayList<>();
     
+    private int total = 0;
+    private boolean isBlackjack = false;
+    private boolean isSoft = false;
+    
     public void setHand(Card dealt){
         cardValue.add(dealt);
+        calcTotal();
     }
     
-    public boolean hasBlackjack() {
-        return (cardValue.size() == 2 && getTotal() == 21);
-    }
+    public int getTotal() { return total; }
+    public boolean isBlackjack() { return isBlackjack; }
+    public boolean isSoft() { return isSoft; }
     
-    public int getTotal() {
-        int total = 0;
+    private void calcTotal() {
+        total = 0;
+        isSoft = false;
+        isBlackjack = false;
+        
         boolean hasAce = false;
 
         for (int i = 0; i < cardValue.size(); ++i) {
@@ -41,14 +49,20 @@ public class Hand {
             }
         }
         
-        // If there is an ace in the hand and the total is over 21
-        // then treat the ace as a one instead of an eleven by subtracting
-        // ten from the total.
-        if (hasAce && total > 21) {
-            total -= 10;
+        if (hasAce) {
+            if (total > 21) {
+                // If there is an ace in the hand and the total is over 21
+                // then treat the ace as a one instead of an eleven by subtracting
+                // ten from the total.
+                total -= 10;
+            } else {
+                isSoft = true;
+                
+                if ((total == 21) && (cardValue.size() == 2)) {
+                    isBlackjack = true;
+                }
+            }
         }
-
-        return total;
     }
     
     //added this to display cards
