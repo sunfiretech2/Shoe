03c5b8adb743449f82a86e64799908e6aef6a5c7
Ali <Ali@192.168.1.109>
2019-01-15 00:14:33 -0600

    Added todo notes.

--- Shoe/src/shoe/ShoeDriver.java
+++ Shoe/src/shoe/ShoeDriver.java
@@ -14,6 +14,11 @@ public class ShoeDriver {
         BlackJackGame game = new BlackJackGame();
         System.out.println(game.shoe.myShoeCard.size());
         while(true){
+            
+            // ALI 01/14/2019
+            // Let's rename startGame to playGame and at the end of playGame,
+            // call a method gameEnd() and move clearing the hands to that
+            // method.
             game.startGame();
             
             //revised hand class arraylist to protected
