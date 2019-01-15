f7c0bd68df01160a6553a7c212d7e225176ff5de
Ali <Ali@192.168.1.109>
2019-01-15 00:18:24 -0600

    Added todo notes.

--- Shoe/src/shoe/ShoeDriver.java
+++ Shoe/src/shoe/ShoeDriver.java
@@ -10,11 +10,12 @@ public class ShoeDriver {
 
     public static void main(String[] args) {
        
-        //creating  blackjack game
+        // Create blackjack game.
         BlackJackGame game = new BlackJackGame();
+        
         System.out.println(game.shoe.myShoeCard.size());
-        while(true){
-            
+        
+        while(true) {
             // ALI 01/14/2019
             // Let's rename startGame to playGame and at the end of playGame,
             // call a method gameEnd() and move clearing the hands to that
@@ -25,8 +26,8 @@ public class ShoeDriver {
             //revised blackjackgame player, dealer, shoe to protected
             game.player.hand.cardValue.clear();
             game.dealer.hand.cardValue.clear();
+            
             System.out.println(game.shoe.myShoeCard.size());        
         }
-        
     }
 }
